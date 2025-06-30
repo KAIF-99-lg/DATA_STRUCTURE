0a12b7116acf1ce215ad1247efcaab9fb2470323
Md Kaif <k82102002@gmail.com>
2025-06-30 16:28:50 +0530

    Time: 1 ms (83.73%), Space: 41.4 MB (98.72%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int numDecodings(String s) {
+        if(s==null || s.length()==0 || s.charAt(0)=='0') return 0;
+
+        int prev=1;
+        int curr=1;
+
+        for(int i=1;i<s.length();i++){
+            int temp=curr;
+
+            if(s.charAt(i)=='0'){
+                if (s.charAt(i - 1) == '1' || s.charAt(i - 1) == '2') {
+                curr=prev;
+            }else{
+                return 0;
+            }
+        }else if(s.charAt(i-1)=='1' || s.charAt(i-1)=='2' && s.charAt(i)<='6'){
+            curr=curr+prev;
+        }
+        prev=temp;
+    }
+    return curr;
+
+}
+}
